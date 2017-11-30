9c71d1ee85c18487b599ad2d71ecf95e2f0f963f
zhaoning <zhaoningdeemail@qq.com>
2017-12-01 00:14:07 +0800

    提交彩票页面部分代码

--- DDAYGO/ViewController/My/Lottery/LotteryHomePage/LotteryController.h
+++ DDAYGO/ViewController/My/Lottery/LotteryHomePage/LotteryController.h
@@ -7,10 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
-
+/*
+ prize
+ The winning number
+ The sum of the awards in the current period
+ Number of winners
+ Bounty
+ */
 @interface LotteryController : UIViewController 
 @property (nonatomic, strong) UILabel * TitleLabel;  //标题
 @property (nonatomic, strong) UILabel * NumberingLabel; // 编号
 @property (nonatomic, strong) UILabel * DatesLabel; // 日期
+@property (weak, nonatomic) IBOutlet UIScrollView *zhongjiangScrollView;    //中间滚动图
+@property (weak, nonatomic) IBOutlet UILabel *prizeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *winningNumbLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currentPeriodLabel;
+@property (weak, nonatomic) IBOutlet UILabel *winnersNumLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bountyLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *prizeWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *winningNumbWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *currentPeriodWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *winnersNumWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bountyWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *onScrollViewWidth;
+
 
 @end
--- DDAYGO/ViewController/My/Lottery/LotteryHomePage/LotteryController.m
+++ DDAYGO/ViewController/My/Lottery/LotteryHomePage/LotteryController.m
@@ -15,7 +15,7 @@
 @interface LotteryController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UITableView * tableView;
-
+@property (strong ,nonatomic) NSArray *titleArray;
 
 @end
 
@@ -28,6 +28,59 @@
     self.title = NSLocalizedString(@"赏金彩", nil);
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:ZP_WhiteColor}];   // 更改导航栏字体颜色
     [self.navigationController.navigationBar lt_setBackgroundColor:ZP_NavigationCorlor];  //  更改导航栏颜色
+    self.titleArray = @[@"奖项",@"对中奖号数",@"本期各奖项金额",@"中奖数",@"赏金金额"];
+}
+
+- (CGSize)getStringSize:(NSString *)string FontSize:(NSInteger)fontSize Size:(CGSize)size {
+    CGSize size1;
+    CGSize widthSize = [string boundingRectWithSize:size options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName :[UIFont systemFontOfSize:fontSize]} context:nil].size;
+    size1.width = widthSize.width+1;
+    size1.height = widthSize.height+1;
+    return size1;
+}
+
+- (void)updateViewConstraints {
+    [super updateViewConstraints];
+    CGFloat onScrollViewWidth = 0;
+    for (int i = 0; i < self.titleArray.count; i ++) {
+        switch (i) {
+            case 0:
+            {
+                self.prizeWidth.constant = [self getStringSize:self.prizeLabel.text FontSize:13 Size:CGSizeMake(MAXFLOAT, MAXFLOAT)].width + 16;
+                onScrollViewWidth += self.prizeWidth.constant;
+            }
+                break;
+            case 1:
+            {
+                self.winningNumbWidth.constant = [self getStringSize:self.winningNumbLabel.text FontSize:13 Size:CGSizeMake(MAXFLOAT, MAXFLOAT)].width + 16;
+                onScrollViewWidth += self.winningNumbWidth.constant;
+            }
+                break;
+            case 2:
+            {
+                self.currentPeriodWidth.constant = [self getStringSize:self.currentPeriodLabel.text FontSize:13 Size:CGSizeMake(MAXFLOAT, MAXFLOAT)].width + 16;
+                onScrollViewWidth += self.currentPeriodWidth.constant;
+            }
+                break;
+            case 3:
+            {
+                self.winnersNumWidth.constant = [self getStringSize:self.winnersNumLabel.text FontSize:13 Size:CGSizeMake(MAXFLOAT, MAXFLOAT)].width + 16;
+                onScrollViewWidth += self.winnersNumWidth.constant;
+            }
+                break;
+            case 4:
+            {
+                self.bountyWidth.constant = [self getStringSize:self.bountyLabel.text FontSize:13 Size:CGSizeMake(MAXFLOAT, MAXFLOAT)].width + 16;
+                onScrollViewWidth += self.bountyWidth.constant;
+            }
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    self.onScrollViewWidth.constant = onScrollViewWidth;
 }
 
 - (void)initUI {
