6f9329ad7103b1f8964781642efd952d8cd77f18
Toexport <chibizhangpeng@163.com>
2017-12-15 01:11:48 +0800

    修改购物车
    修改购物车

--- DDAYGO/ViewController/Shopping/ShoppingcCart/ShoppingViewController.m
+++ DDAYGO/ViewController/Shopping/ShoppingcCart/ShoppingViewController.m
@@ -50,14 +50,12 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
-    if (!DD_HASLOGIN) {
-        LogregisterController * viewcontroller = [[LogregisterController alloc] init];
-//        self.hidesBottomBarWhenPushed = YES;
-        [self.navigationController pushViewController:viewcontroller animated:YES];
-        self.hidesBottomBarWhenPushed = NO;
-
-        return;
+        if (![MyViewController sharedInstanceTool].hasRemind) {
+            [MyViewController sharedInstanceTool].hasRemind = YES;
+            LogregisterController *viewcontroller = [[LogregisterController alloc] init];
+            self.hidesBottomBarWhenPushed = YES;
+            [self.navigationController pushViewController:viewcontroller animated:YES];
+            self.hidesBottomBarWhenPushed = NO;
     } else {
 //        self.hidesBottomBarWhenPushed = YES;
         [self allData];
