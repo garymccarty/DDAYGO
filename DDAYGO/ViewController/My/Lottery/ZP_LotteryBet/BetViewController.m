389231475ffdb8a2ba5d44141484d930b089c66f
zhaoning <zhaoningdeemail@qq.com>
2017-12-17 01:29:07 +0800

    剩下的你自己搞了

--- DDAYGO/ViewController/My/Lottery/ZP_LotteryBet/BetViewController.m
+++ DDAYGO/ViewController/My/Lottery/ZP_LotteryBet/BetViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     
     [self initUI];
+    [self initTableHeadView];
 }
 
 - (void)initUI {
@@ -46,18 +47,11 @@
     self.navigationItem.rightBarButtonItem = btn;
 }
 
-// 下注
-- (void)Instruction {
-
-    ZPLog(@"下注");
-}
-
-// 表头
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    UIView *myView = [[UIView alloc]init];
+- (void)initTableHeadView {
+    UIView *myView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, ZP_Width, 40)];
     self.tableView.tableHeaderView = myView; // 表头跟着cell一起滚动
     [myView setBackgroundColor:ZP_Graybackground];
-//     标题1
+    //     标题1
     ZP_GeneralLabel * TitleLabel1 = [ZP_GeneralLabel initWithtextLabel:_TitleLabel1.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
     TitleLabel1.text = @"距2014137期截止:";
     [myView addSubview:TitleLabel1];
@@ -65,11 +59,11 @@
     [TitleLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(myView).offset(55);
         make.top.equalTo(myView).offset(5);
-//        make.width.mas_equalTo(90);
+        //        make.width.mas_equalTo(90);
         make.height.mas_equalTo(15);
     }];
     
-//     标题2
+    //     标题2
     ZP_GeneralLabel * TitleLabel2 = [ZP_GeneralLabel initWithtextLabel:_TitleLabel2.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
     TitleLabel2.text = @"奖池:";
     [myView addSubview:TitleLabel2];
@@ -81,7 +75,7 @@
         make.height.mas_equalTo(15);
     }];
     
-//    时间
+    //    时间
     ZP_GeneralLabel * TimeLabel = [ZP_GeneralLabel initWithtextLabel:_TimeLabel.text textColor:[UIColor redColor] font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
     TimeLabel.text = @"16天22小时45分";
     [myView addSubview:TimeLabel];
@@ -91,7 +85,7 @@
         make.top.equalTo(TitleLabel1).offset(0);
         make.height.mas_offset(15);
     }];
-//  金额
+    //  金额
     ZP_GeneralLabel * AmountLabel = [ZP_GeneralLabel initWithtextLabel:_AmountLabel.text textColor:[UIColor redColor] font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
     AmountLabel.text = @"￥165,973,926";
     [myView addSubview:AmountLabel];
@@ -101,8 +95,8 @@
         make.top.equalTo(TitleLabel2).offset(0);
         make.height.mas_offset(15);
     }];
-
-//    元
+    
+    //    元
     ZP_GeneralLabel * YuanLabel = [ZP_GeneralLabel initWithtextLabel:_YuanLabel.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
     YuanLabel.text = @"元";
     [myView addSubview:YuanLabel];
@@ -111,15 +105,82 @@
         make.top.equalTo(AmountLabel).offset(0);
         make.height.mas_offset(15);
     }];
-    
-    return myView;
 }
 
-//  设置表头高度
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    
-    return 35;
+// 下注
+- (void)Instruction {
+
+    ZPLog(@"下注");
 }
+
+// 表头
+//- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+//    UIView *myView = [[UIView alloc]init];
+//    self.tableView.tableHeaderView = myView; // 表头跟着cell一起滚动
+//    [myView setBackgroundColor:ZP_Graybackground];
+////     标题1
+//    ZP_GeneralLabel * TitleLabel1 = [ZP_GeneralLabel initWithtextLabel:_TitleLabel1.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
+//    TitleLabel1.text = @"距2014137期截止:";
+//    [myView addSubview:TitleLabel1];
+//    _TitleLabel1 = TitleLabel1;
+//    [TitleLabel1 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(myView).offset(55);
+//        make.top.equalTo(myView).offset(5);
+////        make.width.mas_equalTo(90);
+//        make.height.mas_equalTo(15);
+//    }];
+//    
+////     标题2
+//    ZP_GeneralLabel * TitleLabel2 = [ZP_GeneralLabel initWithtextLabel:_TitleLabel2.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
+//    TitleLabel2.text = @"奖池:";
+//    [myView addSubview:TitleLabel2];
+//    _TitleLabel2 = TitleLabel2;
+//    [TitleLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(myView).offset(80);
+//        make.top.equalTo(myView).offset(20);
+//        //        make.width.mas_equalTo(90);
+//        make.height.mas_equalTo(15);
+//    }];
+//    
+////    时间
+//    ZP_GeneralLabel * TimeLabel = [ZP_GeneralLabel initWithtextLabel:_TimeLabel.text textColor:[UIColor redColor] font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
+//    TimeLabel.text = @"16天22小时45分";
+//    [myView addSubview:TimeLabel];
+//    _TimeLabel = TimeLabel;
+//    [TimeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(TitleLabel1).offset(90);
+//        make.top.equalTo(TitleLabel1).offset(0);
+//        make.height.mas_offset(15);
+//    }];
+////  金额
+//    ZP_GeneralLabel * AmountLabel = [ZP_GeneralLabel initWithtextLabel:_AmountLabel.text textColor:[UIColor redColor] font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
+//    AmountLabel.text = @"￥165,973,926";
+//    [myView addSubview:AmountLabel];
+//    _AmountLabel = AmountLabel;
+//    [AmountLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(TitleLabel2).offset(25);
+//        make.top.equalTo(TitleLabel2).offset(0);
+//        make.height.mas_offset(15);
+//    }];
+//
+////    元
+//    ZP_GeneralLabel * YuanLabel = [ZP_GeneralLabel initWithtextLabel:_YuanLabel.text textColor:ZP_textblack font:ZP_TrademarkFont textAlignment:NSTextAlignmentLeft bakcgroundColor:nil];
+//    YuanLabel.text = @"元";
+//    [myView addSubview:YuanLabel];
+//    [YuanLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(AmountLabel).offset(75);
+//        make.top.equalTo(AmountLabel).offset(0);
+//        make.height.mas_offset(15);
+//    }];
+//    
+//    return myView;
+//}
+
+//  设置表头高度
+//- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+//
+//    return 0.1;
+//}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
