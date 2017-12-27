976df74fe2831701b151156212be550433bb0f20
Summer <179400524@qq.com>
2017-12-28 01:36:26 +0800

    完成历史填写部分数据
    完成历史填写部分数据

--- DDAYGO/ViewController/My/Lottery/ZP_HistoryBet/ZP_HistoryHomePage/ZP_HistoryBetCell/ZP_HistoryBetCell.m
+++ DDAYGO/ViewController/My/Lottery/ZP_HistoryBet/ZP_HistoryHomePage/ZP_HistoryBetCell/ZP_HistoryBetCell.m
@@ -7,13 +7,22 @@
 //
 
 #import "ZP_HistoryBetCell.h"
+#import "PrefixHeader.pch"
+#import "ZP_HistoryModel.h"
 @implementation ZP_HistoryBetCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     
-    
 }
-
+- (void)HistoryBet:(ZP_HistoryModel *)model {
+//    ZP_HistoryModel * model = [ZP_HistoryModel mj_objectWithKeyValues:model.pollid];
+    [_button1 setTitle:[model.white1 stringValue] forState:UIControlStateNormal];
+    [_button2 setTitle:[model.white2 stringValue] forState:UIControlStateNormal];
+    [_button3 setTitle:[model.white3 stringValue] forState:UIControlStateNormal];
+    [_button4 setTitle:[model.white4 stringValue] forState:UIControlStateNormal];
+    [_button5 setTitle:[model.white5 stringValue] forState:UIControlStateNormal];
+    [_button6 setTitle:[model.powerball stringValue] forState:UIControlStateNormal];
+}
 
 @end
---
+++ DDAYGO/ViewController/My/Lottery/ZP_HistoryBet/ZP_HistoryHomePage/ZP_HistoryModel/ZP_HistoryModel.m
@@ -0,0 +1,13 @@
+//
+//  ZP_HistoryModel.m
+//  DDAYGO
+//
+//  Created by Summer on 2017/12/28.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#import "ZP_HistoryModel.h"
+
+@implementation ZP_HistoryModel
+
+@end
