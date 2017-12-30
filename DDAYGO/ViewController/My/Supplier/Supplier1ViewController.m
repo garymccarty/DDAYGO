d135d3a0cd84aa77afac0d0c0110e5dda4f50397
Toexport <chibizhangpeng@163.com>
2017-12-30 11:50:48 +0800

    商家申请界面
    商家申请界面

--- DDAYGO/ViewController/My/Supplier/Supplier1ViewController.m
+++ DDAYGO/ViewController/My/Supplier/Supplier1ViewController.m
@@ -12,10 +12,14 @@
 #import "SupplierViewCell2.h"
 #import "PrefixHeader.pch"
 #import "ZP_MyTool.h"
-#import "SupplierView.h"
 @interface Supplier1ViewController ()<UITableViewDelegate, UITableViewDataSource>
 @property(nonatomic, strong)NSArray * array;
 @property (nonatomic, strong)NSArray * postionArray;
+
+@property (nonatomic, strong) NSString *seleStr;
+@property (nonatomic, strong) NSMutableArray *typeNameArray;
+@property (nonatomic, strong) NSMutableArray *typeIdArray;
+
 
 @end
 
@@ -24,6 +28,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initUI];
+    [self SupplierllData];
 }
 
 // UI
@@ -33,13 +38,31 @@
     self.tableView.dataSource = self;
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:ZP_WhiteColor}];   // 更改导航栏字体颜色
     [self.navigationController.navigationBar lt_setBackgroundColor:ZP_NavigationCorlor];  //  更改导航栏颜色
-    static NSString * SupplierID = @"SupplierTableViewCell";
-    [self.tableView registerNib:[UINib nibWithNibName:SupplierID bundle:nil] forCellReuseIdentifier:SupplierID];
+
+    [self.tableView registerNib:[UINib nibWithNibName:@"SupplierTableViewCell" bundle:nil] forCellReuseIdentifier:@"SupplierTableViewCell"];
     [self.tableView registerNib:[UINib nibWithNibName:@"SupplierViewCell2" bundle:nil] forCellReuseIdentifier:@"SupplierViewCell2"];
+    
      _array = [NSArray arrayWithObjects:@"公司名稱:",@"統一編號:",@"公司人數:",@"註冊資本:",@"創立日期:",@"組織形態:",@"公司地址:",@"公司電話:",@"公司傳真(選填):",@"公司網址(選填):",@"聯繫人:",@"聯繫電話:",@"經營項目:",@"合作項目:", nil];
 }
 // 提交按钮
 - (IBAction)SubmitBut:(id)sender {
+    
+    
+    //这里就是你需要的所以数据· 自己填上 提交就知道了
+    for (int i = 0; i < _array.count; i ++) {
+        SupplierTableViewCell * cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:i inSection:0]];
+        if (i == 5) {
+              NSLog(@"%@",_seleStr);
+        }else{
+            NSLog(@"textf = %@",cell.textField.text);
+        }
+
+    }
+    
+   NSString * a = @"199311";
+   NSString * nwqa = [NSString stringWithFormat: @"-%@",a]; //----> newa  是不是  -199311
+    
+    
     [self AllData];
 }
 
@@ -47,21 +70,21 @@
 - (void)AllData {
     NSMutableDictionary * dic = [NSMutableDictionary dictionary];
     dic[@"token"] = Token;
-    dic[@"companyname"] = @"测试测试";
-    dic[@"companycode"] = @"51155455454";
-    dic[@"poeplecount"] = @"100人";
-    dic[@"capital"] = @"100万";
-    dic[@"companydate"] = @"2017-10-11";
-    dic[@"companytype"] = @"1";
-    dic[@"address"] = @"公司地址公司地址公司地址公司地址";
-    dic[@"phone"] = @"141414414441441";
-    dic[@"fax"] = @"4545554545";
-    dic[@"companyuri"] = @"www.iii.com";
-    dic[@"contact"] = @"张鹏";
-    dic[@"contactphone"] = @"16161661616";
-    dic[@"contactemail"] = @"110@qq.com";
-    dic[@"companyproduct"] = @"你好吗";
-    dic[@"projectinfo"] = @"你好你好";
+//    dic[@"companyname"] = @"测试测试";
+//    dic[@"companycode"] = @"51155455454";
+//    dic[@"poeplecount"] = @"100人";
+//    dic[@"capital"] = @"100万";
+//    dic[@"companydate"] = @"20171001";
+//    dic[@"companytype"] = @"1";
+//    dic[@"address"] = @"公司地址公司地址公司地址公司地址";
+//    dic[@"phone"] = @"141414414441441";
+//    dic[@"fax"] = @"4545554545";
+//    dic[@"companyuri"] = @"www.iii.com";
+//    dic[@"contact"] = @"张鹏";
+//    dic[@"contactphone"] = @"16161661616";
+//    dic[@"contactemail"] = @"110@qq.com";
+//    dic[@"companyproduct"] = @"你好吗";
+//    dic[@"projectinfo"] = @"你好你好";
     [ZP_MyTool requestSupplierRequest:dic success:^(id obj) {
         ZPLog(@"%@",obj);
     } failure:^(NSError *error) {
@@ -74,40 +97,60 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    static NSString * SupplierID = @"SupplierTableViewCell";
-    SupplierTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:SupplierID];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;  //取消Cell点击变灰效果、
-    cell.textField.placeholder = nil;
-    if (2 == indexPath.row) {
-        cell.textField.placeholder = NSLocalizedString(@"如:50 - 100 人", nil);
-    }
-    if (4 == indexPath.row) {
-        cell.textField.placeholder = NSLocalizedString(@"yyyy-MM-dd", nil);
-    }
+    
+    
     if (5 == indexPath.row) {
         SupplierViewCell2 * cell2 = [tableView dequeueReusableCellWithIdentifier:@"SupplierViewCell2"];
         cell2.selectionStyle = UITableViewCellSelectionStyleNone;  //取消Cell点击变灰效果、
-        cell2.TissueMorphologyLabel.text = @"请选择组织形态";
-        cell2.finishBlock = ^(id response) {
-            [self SupplierllData];
-            NSLog(@"位置");
-            PositionView * position = [[PositionView alloc]initWithFrame:CGRectMake(0, 0, ZP_Width, ZP_height)];
-            //数据
-            [position Position:_postionArray];
-            //返回
-            position.ThirdBlock = ^(NSString *ContStr,NSNumber *code) {
-                NSLog(@"c = %@",ContStr);
-            };
-            //  显示
-            [position showInView:self.navigationController.view];
-        };
+        
+        if (_seleStr.length > 0) {
+            cell2.TissueMorphologyLabel.text = _seleStr;
+        }else{
+            cell2.TissueMorphologyLabel.text = @"请选择组织形态";
+            
+        }
+        [cell2.SelectBut addTarget:self action:@selector(actBut:) forControlEvents:UIControlEventTouchUpInside];
         return cell2;
+    }else {
+        SupplierTableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"SupplierTableViewCell"];
+
+        cell.titleLabel.text = self.array[indexPath.row];
+        if (indexPath.row == 1) {
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+            
+        }
+        if (indexPath.row == 2) {
+            cell.textField.placeholder = NSLocalizedString(@"如:50 - 100 人", nil);
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+        }
+        if (indexPath.row == 3) {
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+        }
+        if (indexPath.row == 4) {
+            cell.textField.placeholder = NSLocalizedString(@"yyyy-MM-dd", nil);
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+        }
+        if (indexPath.row == 7) {
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+        }
+        if (indexPath.row == 8) {
+            cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+            return cell;
+        }
+        
+        if (indexPath.row == 10) {
+            cell.textField.placeholder = NSLocalizedString(@"聯繫人/職稱/分機", nil);
+            return cell;
+        }
+        
+        return cell;
+        
     }
-    if (10 == indexPath.row) {
-        cell.textField.placeholder = NSLocalizedString(@"聯繫人/職稱/分機", nil);
-    }
-    cell.titleLabel.text = self.array[indexPath.row];
-    return cell;
 }
 
 // 获取组织形态列表
@@ -116,28 +159,57 @@
     dic[@"countrycode"] = @"886";
     [ZP_MyTool requestCompanyType:dic success:^(id obj) {
         ZPLog(@"%@",obj);
+        NSArray *ar = obj;
+        [ar enumerateObjectsUsingBlock:^(id  _Nonnull objj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [self.typeIdArray addObject:objj[@"typeid"]];
+            [self.typeNameArray addObject:objj[@"typename"]];
+        }];
     } failure:^(NSError * error) {
         ZPLog(@"%@",error);
     }];
     
 }
+
+- (void)actBut:(UIButton *)but{
+    
+    
+    JXPopoverView *popoverView = [JXPopoverView popoverView];
+    NSMutableArray *titleArray = [NSMutableArray array];
+    for (int i = 0; i < self.typeNameArray.count; i ++) {
+        JXPopoverAction *action1 = [JXPopoverAction actionWithTitle:self.typeNameArray[i] handler:^(JXPopoverAction *action) {
+            
+            NSLog(@"dian ji l %@",self.typeNameArray[i]);
+           
+            _seleStr = self.typeNameArray[i];
+            NSIndexPath *index = [NSIndexPath indexPathForRow:5 inSection:0];
+            [self.tableView reloadRowsAtIndexPaths:[NSArray arrayWithObjects:index, nil] withRowAnimation:UITableViewRowAnimationNone];
+            
+                }];
+        [titleArray addObject:action1];
+    }
+
+    [popoverView showToView:but withActions:titleArray];
+    
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 60;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath  {
-    SupplierView * position = [[SupplierView alloc]initWithFrame:CGRectMake(0, 0, ZP_Width, ZP_height)];
-    [self SupplierllData];
-    //数据
-//    [position Position:_postionArray];
-    //返回
-    position.ThirdBlock = ^(NSString *ContStr,NSNumber *code) {
-        NSLog(@"c = %@",ContStr);
-        //            [indexPath setTitle:NSLocalizedString(ContStr, nil) forState:UIControlStateNormal];
-    };
-    //  显示
-    [position showInView:self.navigationController.view];
-    ZPLog(@"%ld",(long)indexPath.row);
+
+- (NSMutableArray *)typeIdArray
+{
+    if (!_typeIdArray) {
+        _typeIdArray = [NSMutableArray array];
+    }
+    return _typeIdArray;
+}
+- (NSMutableArray *)typeNameArray
+{
+    if (!_typeNameArray) {
+        _typeNameArray = [NSMutableArray array];
+    }
+    return _typeNameArray;
 }
 
 @end
--- DDAYGO/ViewController/My/Supplier/SupplierCell/SupplierViewCell2.m
+++ DDAYGO/ViewController/My/Supplier/SupplierCell/SupplierViewCell2.m
@@ -7,7 +7,6 @@
 //
 
 #import "SupplierViewCell2.h"
-#import "SupplierView.h"
 #import "PrefixHeader.pch"
 @implementation SupplierViewCell2
 
@@ -17,18 +16,7 @@
 }
 
 
-- (IBAction)SelectBut:(UIButton *)sender {
-    SupplierView * position = [[SupplierView alloc]initWithFrame:CGRectMake(0, 0, ZP_Width, ZP_height)];
-    if (self.finishBlock) {
-        self.finishBlock(position);
-        //数据
-        //    [position Position:_postionArray];
-        //返回
-        position.ThirdBlock = ^(NSString *ContStr,NSNumber *code) {
-            NSLog(@"c = %@",ContStr);
-        };
-    }
-    [[NSNotificationCenter defaultCenter]postNotificationName:@"position" object:nil];
-}
+
+
 
 @end
